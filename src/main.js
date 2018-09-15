9ec56c93bc1d11ee22f204c95f18d170594486f3
Mateus Nakajo <mateusnakajo@gmail.com>
2018-09-15 19:41:12 -0300

    remove bootstrap

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
