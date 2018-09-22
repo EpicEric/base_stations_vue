a80527e32a762beba185a8978fa7dc6468afb17a
Mateus Nakajo <mateusnakajo@gmail.com>
2018-09-22 20:38:36 -0300

    Add a signup page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Signup from './views/Signup.vue'
+
 
 Vue.use(Router)
 
@@ -17,6 +19,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
     }
   ]
 })
---
+++ src/views/Signup.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1>Cadastro</h1>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <form>
+          <v-layout column>
+            <v-flex>
+              <v-text-field
+                name="usuario"
+                label="Usuário"
+                id="usuario"
+                type="text"
+                required></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                name="password"
+                label="Senha"
+                id="password"
+                type="password"
+                required></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                name="confirmPassword"
+                label="Confirmar senha"
+                id="confirmPassword"
+                type="password"
+                required></v-text-field>
+            <v-text-field
+                name="email"
+                label="Email"
+                id="email"
+                type="email"
+                required></v-text-field>
+            <v-text-field
+                name="confirmEmail"
+                label="Confirmar email"
+                id="confirmEmail"
+                type="email"
+                required></v-text-field>
+            </v-flex>
+            <v-flex class="text-xs-center" mt-5>
+              <v-btn color="primary" type="submit">Cadastrar</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
