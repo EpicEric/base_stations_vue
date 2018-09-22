af5c7cfad604fccdf83c182bbee0fe3509af5601
Mateus Nakajo <mateusnakajo@gmail.com>
2018-09-22 19:55:53 -0300

    Add simple footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="footer">
-        Footer
-    </div>
+  <v-footer class="pa-3">
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
 </template>
