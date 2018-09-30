6e6f97f0f686ad7d23b470ad7335907e26f38c17
Mateus Nakajo <mateusnakajo@gmail.com>
2018-09-29 23:16:40 -0300

    Add leaftlet example

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -3,7 +3,7 @@
     <MapSidebar></MapSidebar>
     <v-content>
       <v-container fluid>
-        <div>Beautiful Map</div>
+        <div id="map"></div>
       </v-container>
     </v-content>
   </div>
@@ -14,6 +14,20 @@ import MapSidebar from '@/components/MapSidebar'
 export default {
   components: {
     MapSidebar
+  },
+  mounted() {
+      var map = L.map('map').setView([51.505, -0.09], 13)
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+
+      L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup()
   }
 }
 </script>
+<style>
+#map {height: 1000px; }
+</style>
