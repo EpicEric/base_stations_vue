70770128f684be2f8c8e2a0a47f0e0c631ba19c0
Eric Rodrigues Pires <ericpires9@gmail.com>
2018-12-14 17:14:09 -0200

    Improve login screen
    Closes #1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,23 @@
 <template>
-  <v-container>
-    <v-layout row class="text-xs-center">
-      <v-flex xs4 class="grey lighten-4">
-        <v-container style="position: relative;top: 15%;" class="text-xs-center">
-          <v-card flat>
-            <v-card-title primary-title>
-              <h4>Login</h4>
-            </v-card-title>
+  <v-container fluid fill-height v-on:keyup.enter="login">
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark>
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
             <v-form>
-            <v-text-field prepend-icon="person" v-model="email" name="Email" label="Email"></v-text-field>
-            <v-text-field prepend-icon="lock" v-model="password" name="Senha" label="Senha" type="password"></v-text-field>
-            <v-card-actions>
-              <v-btn primary large block @click="login">Login</v-btn>
-            </v-card-actions>
+              <v-text-field prepend-icon="person" v-model="email" name="login" label="Login" type="text"></v-text-field>
+              <v-text-field id="password" prepend-icon="lock" v-model="password" name="password" label="Password" type="password"></v-text-field>
             </v-form>
-          </v-card>
-        </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <span class="red--text caption" v-if="!valid">Usuário ou senha inválidos.</span>
+            <v-spacer></v-spacer>
+            <v-btn type="submit" dark @click="login">Login</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -27,7 +29,8 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      valid: true
     }
   },
   computed: {
@@ -40,9 +43,12 @@ export default {
       'loginAction'
     ]),
     async login () {
+      this.valid = true
       await this.loginAction({ username: this.email, password: this.password })
       if (this.isAuthenticated) {
         this.$router.push('map')
+      } else {
+        this.valid = false
       }
     }
   }
